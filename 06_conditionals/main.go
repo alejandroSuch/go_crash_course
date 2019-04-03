877667a555c9ea1c3f57992180bd6f4b22274227
Alejandro Such <alejandro.such@gmail.com>
2019-04-03 07:19:23 +0200

    add switch

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	x := 5
+	x := 10
 	y := 10
 
 	if x < y {
 		fmt.Printf("%d is lower than %d\n\n", x, y)
+	} else {
+		fmt.Printf("%d is greater than or equals %d\n\n", x, y)
+	}
+
+	color := "orange"
+
+	switch color {
+	case "red":
+		fmt.Println("Color is red")
+	case "blue":
+		fmt.Println("Color is blue")
+	default:
+		fmt.Println("Color is not red nor blue")
+
 	}
 }
